f1624d60e9a51a1cfc7190b50dc412c4b2586185
Harald Schilly <harald.schilly@univie.ac.at>
2012-01-19 23:07:43 +0100

    setuptools setup.py, starting to work on it

--- setup.py
+++ setup.py
@@ -1,78 +1,31 @@
 #!/usr/bin/env python
 
-import os
-import sys
-import numpy
-from numpy.distutils.misc_util import Configuration
-from numpy.distutils.core      import setup
+from setuptools import setup, find_packages
 
-# -----------------------------------------------------
-if len(sys.argv) == 1:
-    sys.argv.append('install')
+from psnobfit import __version__
+setup(
+    name = "psnobfit",
+    version = __version__, 
+    packages = find_packages(),
 
-# BEFORE importing distutils, remove MANIFEST. distutils doesn't properly
-# update it when the contents of directories change.
-if os.path.exists('MANIFEST'):
-    os.remove('MANIFEST')
+    # Project uses reStructuredText, so ensure that the docutils get
+    # installed or upgraded on the target machine
+    install_requires = ['docutils>=0.3', "numpy >= 1.5.0", "IPython >= 0.12" ],
 
+    package_data = {
+        # If any package contains *.txt or *.rst files, include them:
+        '': ['*.txt', '*.rst']#,
+    #    # And include any *.msg files found in the 'hello' package, too:
+    #    'hello': ['*.msg'],
+    },
 
-# ------------------------------------------------------
-def configuration(parent_package='',
-                  top_path=None
-                  ):
-    if  numpy.__version__ < '1.0.0':
-        raise RuntimeError, 'numpy version %s or higher required, but got %s'\
-              % ('1.0.0', numpy.__version__)
+    # metadata for upload to PyPI
+    author           = "Harald Schilly",
+    author_email     = "harald.schilly@univie.ac.at",
+    description      = "Parallel Snobfit",
+    url              = "",
+    license          = 'BSD',
+    keywords = "optimization blackbox stochastic noisy parallel",
 
-    if IPython.__version__ < '0.12':
-        raise RuntimeError, 'IPython version %s or higher required, but got %s'\
-              % ('1.0.0', IPython.__version__)
-   
-    
-    config = Configuration(None, parent_package, top_path)
-
-    config.set_options( ignore_setup_xxx_py=True,
-                        assume_default_configuration=True,
-                        delegate_options_to_subpackages=True,
-                        quiet=True
-                       )
-     
-    config.add_subpackage('psnobfit')
-    
-    config.add_data_files( ('psnobfit', '*.txt') )
-    config.add_data_files( ('psnobfit', 'psnobfit.epydoc') )
-    #config.get_version(os.path.join('snobfit','version.py'))   # sets config.version
-
-    return config
-
-
-
-def setup_package():
-    old_path   = os.getcwd()
-    local_path = os.path.dirname(os.path.abspath(sys.argv[0]))
-    os.chdir(local_path)
-    sys.path.insert(0,local_path)
-    sys.path.insert(0,os.path.join(local_path, 'snobfit')) # to retrive version
-
-    try:
-        from version import version as version
-        setup( name = 'üsnobfit',
-               version = version, #will be overwritten by configuration version
-               maintainer       = "Harald Schilly",
-               maintainer_email = "harald@schil.ly",
-               description      = "Parallel Snobfit",
-               url              = "",
-               license          = 'BSD',
-               configuration=configuration
-               )
-    finally:
-        del sys.path[0]
-        os.chdir(old_path)
-
-    return
-
-
-# -----------------------------------------------------
-if __name__ == '__main__':
-    setup_package()
-
+    # could also include long_description, download_url, classifiers, etc.
+)

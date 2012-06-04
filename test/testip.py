9725737135042762d543c7df758c99fa49c21ddf
Harald Schilly <harald@schil.ly>
2012-06-04 17:09:22 +0200

    testip: does some kind of optimization based on random sampling

--- test/testip.py
+++ test/testip.py
@@ -7,7 +7,8 @@ import itertools as it
 # setup proper logging
 import logging
 logger = logging.getLogger('psnobfit')
-logger.setLevel(logging.INFO) #DEBUG)
+logger.setLevel(logging.INFO)
+#logger.setLevel(logging.DEBUG)
 log_stream_handler = logging.StreamHandler()
 log_stream_handler.setLevel(logging.DEBUG)
 log_formatter = logging.Formatter('%(asctime)s %(name)s/%(levelname)-9s %(message)s')
@@ -24,16 +25,16 @@ options, args = opt_parser.parse_args()
 
 # START
 logger.info("init")
-from IPython.parallel import Client
+from IPython.parallel import Client, require
 c = Client(profile=options.client_profile)
 c.clear() # clears remote engines
 c.purge_results('all') # all results are memorized in the hub
 lb = c.load_balanced_view()
 
 # MAX number of tasks in total
-MAX = 500
+MAX = 5000
 # length of test data, sent over the wire
-DSIZE = 1000
+DIMSIZE = 50
 # when adding machines, this is the number of additional tasks
 # beyond the number of free machines
 new_extra = 2
@@ -44,25 +45,38 @@ with c[:].sync_imports():
   import time
   from random import random
   from numpy import pi, sum
+  import numpy
   import math
 
 # the actual function
-def func(tid, data):
+def func_sum(tid, data):
   'x is either a number or a list/vector of numbers'
-  time.sleep(math.log(2 + random()))
-  return tid, sum(data)
+  time.sleep(math.log(1 + random()))
+  return tid, numpy.sum(data)
+
+def func_eval(tid, data):
+  np = numpy
+  v = np.dot(np.cos(numpy.pi + data), np.sin(data + numpy.pi/2))
+  v = np.exp(np.abs(v) / len(data))
+  #s = np.sin(data[::2])
+  #c = np.cos(data[1::2])
+  #v = np.prod(np.append(s,c))
+  return tid, v
+
+func = func_eval
 
 # some stats
 added       = 0
 queue_size  = 0
 added       = 0
 nb_finished = 0
-cum_sum     = 0
 loops       = 0
 tasks_added = 0
+best_x      = None
+best_obj    = numpy.infty
 
 def status():
-  logger.info("pending %4d | + %2d tasks | total added: %4d | finished: %4d | cum_sum: %.2f" % (queue_size, new, added, nb_finished, cum_sum))
+  logger.info("pending %4d | + %2d tasks | total: %4d | finished: %4d | best_obj: %.10f" % (queue_size, new, added, nb_finished, best_obj))
 
 logger.info("start")
 
@@ -71,8 +85,9 @@ pending = set([])
 pending_ts = []
 # collects all returns
 results = []
+allx = dict() # store all x vectors
 
-while pending or added < MAX: #cum_sum < MAX *  DSIZE / 2:
+while pending or added < MAX: 
   lb.spin() # check outstanding tasks
   loops += 1
 
@@ -88,8 +103,9 @@ while pending or added < MAX: #cum_sum < MAX *  DSIZE / 2:
     added += new
     status()
     # create new tasks
-    tids, vals = range(now, now+new), [ np.random.rand(DSIZE) for _ in range(new) ]
-    newt = lb.map_async(func, tids, vals, chunksize=1)
+    tids, vals = range(now, now+new), np.array([ 2 * np.random.rand(DIMSIZE) for _ in range(new) ])
+    newt = lb.map_async(func, tids, vals, chunksize=1, ordered=False)
+    allx.update(zip(tids, vals))
     map(pending.add, newt.msg_ids)
   else:
     new = 0
@@ -114,8 +130,10 @@ while pending or added < MAX: #cum_sum < MAX *  DSIZE / 2:
       # even if chunksize == 1
       t = res.result[0]
       logger.debug("result '%s' = %s" % (msg_id, t))
-      cum_sum += t[1]
       results.append(t)
+      if t[1] < best_obj:
+        best_obj = t[1]
+        best_x   = allx[t[0]]
 
   lb.wait(pending, 1e-3)
 
@@ -128,9 +146,12 @@ logger.debug("pending: %s" % pending)
 logger.info("added in total:   %s" % added)
 
 #logger.info("results: %s" % sorted([r[0] for r in results]))
-logger.info("nb of machines = %s" % len(c.ids))
-logger.info("nb of results = %s | cum_sum = %.2f" % (len(results), cum_sum))
-logger.info("nb of total loops %s | of that, %s times tasks were added | %.4f%%" % (loops, tasks_added, tasks_added / float(loops) * 100.))
+logger.info("# machines = %s" % len(c.ids))
+logger.info("# results = %s" % len(results))
+logger.info("# total loops %s | of that, %s times tasks were added | %.4f%%" % (loops, tasks_added, tasks_added / float(loops) * 100.))
+logger.info("best:")
+logger.info(" obj. value: %f" % best_obj)
+logger.info(" x:\n%s" % best_x)
 logger.info("finished")
 
 #logger.info("pending_ts: %s" % pending_ts)

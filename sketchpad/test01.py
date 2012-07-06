6c9b5a741fd84cbda2d4b869daa77e23137048c3
Harald Schilly <harald.schilly@univie.ac.at>
2012-07-06 18:30:28 +0200

    example problems himmelblau and stochastic rosenbrock added

--- sketchpad/test01.py
+++ sketchpad/test01.py
@@ -35,6 +35,34 @@ class RosenbrockAbs(Problem):
     return sum(self.par1 * np.abs(x[1:] - np.abs(x[:-1])) + \
                np.abs(1-x[:-1]))
     
+class RosenbrockStochastic(Problem):
+  '''
+  f(x) = sum_i (100 eps_i (x_{i+1} - x_i^2)^2 + (1-x_i)^2)
+
+  where eps_i is uniformly random in [0, 1)
+  '''
+  def __init__(self, dims, par1 = 100):
+    box = [(-2,2)] * dims
+    box[0] = (0,2) # for cornercases + testing
+    self.par1 = par1
+    Problem.__init__(self, box)
+
+  def eval(self, x):
+    eps = np.random.rand(self.dim - 1)
+    return sum(self.par1 * eps * (x[1:] - x[:-1]**2)**2 + (1-x[:-1])**2)
+
+class Himmelblau(Problem):
+  '''
+  f(x,y) = (x^2+y-11)^2 + (x+y^2-7)^2
+
+  Lit. http://en.wikipedia.org/wiki/Himmelblau%27s_function
+  '''
+  def __init__(self):
+    Problem.__init__(self, [(-5,5)] * 2)
+
+  def eval(self, x):
+    x, y = x[0], x[1]
+    return (x**2+y-11)**2+(x+y**2-7)**2
 
 class Rastrigin(Problem):
   '''

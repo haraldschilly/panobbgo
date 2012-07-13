73bcd9f13928ac51fcd4e6891d1321db75846724
Harald Schilly <harald.schilly@univie.ac.at>
2012-07-13 22:45:55 +0200

    Strategy0 + Collector as classes and separate threads work; stats need to be hooked up

--- panobbgo/strategies.py
+++ panobbgo/strategies.py
@@ -4,27 +4,28 @@ import threading
 from utils import stats, logger
 from IPython.parallel import Client, Reference #, require, Reference
 from IPython.utils.timing import time
-import numpy as np
+#import numpy as np
 
-def _setup_cluster(nb_gens, problem):
-  c = Client(profile=config.ipy_profile)
-  c.clear() # clears remote engines
-  c.purge_results('all') # all results are memorized in the hub
+class Collector(threading.Thread):
+  '''
+  This Collector receives result lists and passes them along to the statistics and UI.
+  It needs to be terminated via a `None` element.
+  '''
+  def __init__(self):
+    from Queue import Queue
+    self._tasklist = Queue()
+    threading.Thread.__init__(self, name=self.__class__.__name__)
+    self.start()
 
-  if len(c.ids) < nb_gens + 1:
-    raise Exception('I need at least %d clients.' % (nb_gens + 1))
-  dv_evaluators = c[nb_gens:]
-  dv_evaluators['problem'] = problem
-  generators = c.load_balanced_view(c.ids[:nb_gens])
-  evaluators = c.load_balanced_view(c.ids[nb_gens:])
+  def run(self):
+    while True:
+      tlist = self.tasklist.get()
+      if tlist is None: return
+      for t in tlist:
+        logger.info("%s by %s" % (t, t.point.who))
 
-  # import some packages  (also locally)
-  #with c[:].sync_imports():
-  #  from IPython.utils.timing import time
-  #  import numpy
-  #  import math
-  return generators, evaluators
-
+  @property
+  def tasklist(self): return self._tasklist
 
 class Strategy0(threading.Thread):
   def __init__(self, problem, results, heurs):
@@ -32,10 +33,27 @@ class Strategy0(threading.Thread):
     self.results = results
     self.heurs = heurs
     self._setup_cluster(1, problem)
-    threading.Thread.__init__(self, name="Strategy0")
+    self.collector = Collector()
+    self.tasklist = self.collector.tasklist
+    threading.Thread.__init__(self, name=self.__class__.__name__)
 
   def _setup_cluster(self, nb_gens, problem):
-    self.generators, self.evaluators = _setup_cluster(nb_gens, problem)
+    c = Client(profile=config.ipy_profile)
+    c.clear() # clears remote engines
+    c.purge_results('all') # all results are memorized in the hub
+
+    if len(c.ids) < nb_gens + 1:
+      raise Exception('I need at least %d clients.' % (nb_gens + 1))
+    dv_evaluators = c[nb_gens:]
+    dv_evaluators['problem'] = problem
+    self.generators = c.load_balanced_view(c.ids[:nb_gens])
+    self.evaluators = c.load_balanced_view(c.ids[nb_gens:])
+
+    # import some packages  (also locally)
+    #with c[:].sync_imports():
+    #  from IPython.utils.timing import time
+    #  import numpy
+    #  import math
 
   def run(self):
     fff = Reference("problem")
@@ -43,18 +61,18 @@ class Strategy0(threading.Thread):
       points = []
       while len(points) < 10:
         for h in self.heurs:
-          points.extend(h.get_points(50))
+          points.extend(h.get_points(1))
         if len(points) == 0:
           time.sleep(1e-3)
 
-      new_tasks = self.evaluators.map_async(fff, points, chunksize = 3, ordered=False)
+      new_tasks = self.evaluators.map_async(fff, points, chunksize = 5, ordered=False)
       new_tasks.wait()
-      for t in new_tasks:
-        logger.info("run %2d: %s by %s" % (run, t, t.point.who))
+      self.tasklist.put(new_tasks)
 
+    # signal to end
+    self.tasklist.put(None)
+    self.collector.join()
     print "Strategy0 finished"
-
-
 
 
 def strategy_bare(problem, results, heurs, nb_gens=1):

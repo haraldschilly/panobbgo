fb207fb45cdb1f02b792f7346dc8107b4496d87d
Harald Schilly <harald.schilly@univie.ac.at>
2012-09-07 13:17:54 +0200

    doc.sh file

---
+++ doc.sh
@@ -0,0 +1,18 @@
+#!/usr/bin/env bash
+
+set -e
+
+cd `dirname "$0"`
+
+cd doc
+
+COMMIT=`git rev-parse HEAD`
+
+make clean
+make html
+make latexpdf
+
+cd build
+git add -A
+git commit -m "doc for $COMMIT"
+git push origin gh-pages

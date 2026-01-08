87875f8109606ff095e675d315f086eb39a2842e
Harald Schilly <harald.schilly@univie.ac.at>
2026-01-08 12:02:05 +0100

    Fix rmatrix.py for Python 3 compatibility
    - Change 'print m' to 'print(m)' for Python 3 syntax
    - Format file with ruff
    - Update CI to include rmatrix.py in formatting checks

--- sketchpad/rmatrix.py
+++ sketchpad/rmatrix.py
@@ -11,13 +11,13 @@ full = float(m.shape[0] * m.shape[1])
 sqrt3 = np.sqrt(3)
 sign = 1
 
-while len(m) / full <= .1:
+while len(m) / full <= 0.1:
     i = np.random.randint(m.shape[0])
     j = np.random.randint(m.shape[1])
-    #m[i,j] = np.random.rand()
-    if np.random.rand() < .5:
+    # m[i,j] = np.random.rand()
+    if np.random.rand() < 0.5:
         m[i, j] = sqrt3
     else:
-        m[i, j] = - sqrt3
+        m[i, j] = -sqrt3
 
-print m
+print(m)
